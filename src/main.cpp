02453ed255ac80e0d6b4c248af360dfc205b3604
Lana Sprajc <lana.sprajc@fer.hr>
2023-05-08 19:13:31 +0200

    testing updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,9 @@
-#include"tokenizer/tokenizer.hpp"
 #include<iostream>
+#include "lexer/lexer.hpp"
 
 int main(int argc, char** argv)
 {
     std::string file = argv[1];
     tokenizer *t = new tokenizer(file);
-    Token *a;
-    do
-    {
-        a = t->get_next_token();
-        std::cout << a->type << " " << a->line << " " << a->value << std::endl;
-    } while(a->value.size() != 0);
+    program(t);
 }
