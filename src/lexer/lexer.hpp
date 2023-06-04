ee70f841894a30c866e13dd8996dbcf504e9e709
Lana Sprajc <lana.sprajc@fer.hr>
2023-06-04 13:01:15 +0200

    move records in separate file

--- src/lexer/lexer.hpp
+++ src/lexer/lexer.hpp
@@ -1,91 +1,9 @@
 #include "../generator/generator.hpp"
 #include "../tokenizer/tokenizer.hpp"
-#include <vector>
-#include <string>
+#include "records.hpp"
+
 
 class lexer;
-
-enum data_types {
-	undefined = -1,
-	bool_type,
-	int8,
-	int16,
-	int32,
-	int64,
-	uint8,
-	uint16,
-	uint32,
-	uint64,
-	float32,
-	float64,
-	float96,
-	string_type,
-	class_,
-	function_,
-	class_instance,
-	array_,
-	array_element
-};
-
-class Scope;
-class Var_object;
-class Variable {
-	private:
-        static inline int generated;
-    public:
-		std::string name;
-		std::string generated_name;
-		data_types type;
-        Variable();
-};
-class Scope {
-	public:
-		std::vector <Variable*> variables;
-		Scope* parent_scope;
-		Scope(Scope* parent_scope_): parent_scope(parent_scope_) {}
-};
-class Function: public Variable {
-	public:
-		Var_object* return_object;
-		Scope* function_scope;
-		std::vector <Var_object* > function_parameters;
-		Function(std::string name_, Scope* scope, Var_object* return_object_);
-		Function(std::string name_, Scope* scope);
-};
-
-class Class: public Variable {
-	public:
-		Scope* class_scope;
-		Function* constructor;
-		std::unordered_set<Class*> base_classes;
-		Class(std::string name_, Scope* scope);
-};
-
-class Var_object: public Variable {
-	public:
-		Class* class_type;
-		Var_object(std::string name_, data_types type_);
-};
-class Array: public Variable {
-	public:
-		std::vector<Var_object*> size;
-		Var_object* containing_type;
-		Array(std::string name_, std::vector<Var_object*> size_, Var_object* type);
-};
-
-class Array_element: public Var_object {
-	private:
-		std::vector<Var_object*> begin_index;
-		std::vector<Var_object*> end_index;
-	public:
-		void add_begin(Var_object* begin);
-		void add_end(Var_object* end);
-		Array* array_;
-		Array_element(Array_element* arr);
-		Array_element(Array* arr);
-		bool is_one_element();
-};
-
 class lexer
 {
 private:
